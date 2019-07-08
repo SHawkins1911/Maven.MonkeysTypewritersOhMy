df25a286576c6bb57614c65f0180111faa585695
Stefun <hawkd215@gmail.com>
2019-07-08 04:25:12 -0400

    done

--- src/main/java/io/zipcoder/MonkeyTypewriter.java
+++ src/main/java/io/zipcoder/MonkeyTypewriter.java
@@ -1,5 +1,7 @@
 package io.zipcoder;
 
+import java.util.stream.Stream;
+
 public class MonkeyTypewriter {
     public static void main(String[] args) {
         String introduction = "It was the best of times,\n" +
@@ -23,7 +25,11 @@ public class MonkeyTypewriter {
         // Do all of the Monkey / Thread building here
         // For each Copier(one safe and one unsafe), create and start 5 monkeys copying the introduction to
         // A Tale Of Two Cities.
+        UnsafeCopier unsafe = new UnsafeCopier(introduction);
+        monkeySeeMonkeyType(unsafe, 5);
 
+        SafeCopier safe = new SafeCopier(introduction);
+        monkeySeeMonkeyType(safe, 5);
 
         // This wait is here because main is still a thread and we want the main method to print the finished copies
         // after enough time has passed.
@@ -34,5 +40,12 @@ public class MonkeyTypewriter {
         }
 
         // Print out the copied versions here.
+        System.out.println(unsafe.copied);
+        System.out.println(safe.copied);
+    }
+
+    public static void monkeySeeMonkeyType(Copier copier, Integer monkeys){
+        Stream<Thread> myStream = Stream.generate(() -> new Thread(copier)).limit(monkeys);
+        myStream.forEach(Thread::start);
     }
 }
--- src/main/java/io/zipcoder/UnsafeCopier.java
+++ src/main/java/io/zipcoder/UnsafeCopier.java
@@ -1,5 +1,8 @@
 package io.zipcoder;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Modify the run function so that the monkeys each grab the next word and write it to the copy.
  */
@@ -10,5 +13,21 @@ public class UnsafeCopier extends Copier {
     }
 
     public void run() {
+        while (stringIterator.hasNext()) {
+            String next = stringIterator.next();
+            Integer[] array = new Integer[9999];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = (int) (Math.random() * ((600000 - 1) + 1));
+
+            }
+
+            try {
+                Thread.sleep((int) (Math.random() * ((100 - 1) + 1)));
+            } catch (InterruptedException ex) {
+            }
+            Arrays.sort(array);
+            Arrays.sort(array, Collections.reverseOrder());
+            copied += next + " ";
+        }
     }
 }
